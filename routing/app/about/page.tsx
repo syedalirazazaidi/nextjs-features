1d39ba65e7d04ebf1f783ed9c0c15e963e4734c3
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-11-28 00:23:27 +0500

    basic routing

---
+++ routing/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+function About() {
+  return (
+    <div className="min-h-screen">
+      <Link href="/">HOME</Link>
+      about page
+    </div>
+  );
+}
+
+export default About;
---
+++ routing/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="min-h-screen">
+      <Link href="/">HOME</Link>
+      contact page
+    </div>
+  );
+}
+
+export default Contact;
