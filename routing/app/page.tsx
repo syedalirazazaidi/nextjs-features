2479f4dc41536c927a7c753db24e0940470d1995
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-11-28 20:54:09 +0500

    completed 4

--- routing/app/page.tsx
+++ routing/app/page.tsx
@@ -1,13 +1,34 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className=" min-h-screen space-x-6 items-center justify-between p-24">
-      <Link href="/about">About us</Link>
+      <h2>route using Link</h2>
+      {/* <Link href="/about">About us</Link>
       <Link href="/contact">Contact us</Link>
       <Link href="/products/allproducts"> Go to ALL Products Route</Link>
-      <Link href="/products/specificproduct">Go to ALL Products Route</Link>
+      <Link href="/products/specificproduct">Go to ALL Products Route</Link> */}
+      <h2>Programatically Route</h2>
+      <div className=" bg-gray-200 flex space-x-10 w-[120px]">
+        <hr />
+        <div
+          className="hover:cursor-pointer hover:bg-teal-500"
+          onClick={() => router.push("/page1")}
+        >
+          Read
+        </div>
+        <div
+          className="hover:cursor-pointer hover:bg-green-500"
+          onClick={() => router.push("/page2")}
+        >
+          Write
+        </div>
+      </div>
     </main>
   );
 }
---
+++ routing/app/page2/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Write() {
+  return <div>write</div>;
+}
+
+export default Write;
