71e64dbbd36d80fe2c0e8ec53a2fc661c850c547
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-11-30 23:29:21 +0500

    change font

--- layout/app/page.tsx
+++ layout/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Comfortaa, Inter, Poppins, Montserrat } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: "500" });
+const montserrat = Montserrat({ subsets: ["latin"], weight: "500" });
+
+// Montserrat
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     basic layout
+      <div className={montserrat.className}>
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form, by injected humour, or
+        randomised words which don't look even slightly believable. If you are
+        going to use a passage of Lorem Ipsum, you need to be sure there isn't
+        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
+        generators on the Internet tend to repeat predefined chunks as
+        necessary, making this the first true generator on the Internet.
+      </div>
     </main>
-  )
+  );
 }
