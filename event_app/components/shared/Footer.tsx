8046a37bdbddd768b921db9e9eed49e18f1d3d4c
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-12-19 08:20:02 +0500

    add layout

--- event_app/components/shared/Footer.tsx
+++ event_app/components/shared/Footer.tsx
@@ -1,4 +1,3 @@
-
 export default function Footer() {
-  return <div>Footer</div>;
+  return <footer className="w-full border-t">Footer</footer>;
 }
--- event_app/components/shared/Header.tsx
+++ event_app/components/shared/Header.tsx
@@ -1,6 +1,28 @@
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/icons/growth.png"
+            width={24}
+            height={20}
+            alt="evently logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
 }
